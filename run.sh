692fae06b5ecd5b566259e4db40745e029863559
Marcial Rosales <mrosales@pivotal.io>
2016-12-01 18:20:47 +0100

    add helper run.sh

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+if [ -e "./credentials.yml" ]; then
+  stub="./credentials.yml"
+else
+  stub=$1; shift
+fi
+
+set -ue
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
+export fly_target=${fly_target:-main}
+echo "Concourse API target ${fly_target}"
+echo "Concourse API $ATC_URL"
+echo "Tutorial $(basename $DIR)"
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+if [[ "${stub}X" == "X" ]]; then
+  echo "USAGE: run.sh path/to/credentials.yml"
+  exit 1
+fi
+stub=$(realpath $stub)
+if [[ ! -f ${stub} ]]; then
+  echo "USAGE: run.sh path/to/credentials.yml"
+  exit 1
+fi
+
+pushd $DIR
+  fly sp -t ${fly_target} configure -c pipeline.yml -p main --load-vars-from ${stub} -n
+  fly -t ${fly_target} unpause-pipeline --pipeline main
+  fly -t ${fly_target} trigger-job -j main/job-build-and-verify
+  fly -t ${fly_target} watch -j main/job-build-and-verify
+popd
