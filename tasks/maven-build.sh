acebfac5c4afa1abb875aa447c756f450e769b0f
Marcial Rosales <mrosales@pivotal.io>
2017-01-12 13:22:33 +0100

    modify script

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -10,4 +10,14 @@ echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
 
 mvn verify ${MAVEN_ARGS}
 
-cp target/*.jar build
+echo "ls ../build"
+ls -l ../build
+
+echo "ls target"
+ls -l target
+
+echo "copy from target to ../build"
+cp target/*.jar ../build
+
+echo "ls ../build"
+ls -l ../build
