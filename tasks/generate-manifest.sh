f4256e968d1beb8dbc0c17aa8e1df8bae0334825
Marcial Rosales <mrosales@pivotal.io>
2017-01-12 19:58:58 +0100

    fix hack by properly generating a manifest with the path

--- tasks/generate-manifest.sh
+++ tasks/generate-manifest.sh
@@ -2,9 +2,16 @@
 
 set -eu # fail if it finds unbound variables
 
+if [ ! -d artifact ]; then
+  echo "artifact folder does not exist"
+  exit 1
+fi
 if [ ! -d manifest ]; then
   mkdir manifest-to-deploy
 fi
+
+cp artifact/* manifest
+APP_PATH=`ls manifest`
 
 cd manifest
 echo "Writing manifest.yml to [manifest/manifest.yml]"
@@ -14,7 +21,7 @@ cat > manifest.yml <<EOF
 applications:
 - name: ${APP_NAME}
   host: ${APP_HOST}
-
+  path: ${APP_PATH}
 EOF
 
 cat manifest.yml
