c9e5a0a34c851bec61f2efb58773cf3285040333
Marcial Rosales <mrosales@pivotal.io>
2017-01-12 19:48:31 +0100

    add some hacks to get it to work first

--- tasks/generate-manifest.sh
+++ tasks/generate-manifest.sh
@@ -2,31 +2,21 @@
 
 set -eu # fail if it finds unbound variables
 
-if [ ! -d artifact-to-deploy ]; then
-  echo "folder artifact-to-deploy must exist";
-  exit 1
-fi
-if [ ! -d manifest-and-artifact ]; then
+if [ ! -d manifest ]; then
   mkdir manifest-to-deploy
 fi
 
-cd artifact-to-deploy
-APP_PATH=`ls`
-cd ..
-
-cd manifest-and-artifact
-echo "Writing manifest.yml to [manifest-to-deploy/manifest.yml]"
+cd manifest
+echo "Writing manifest.yml to [manifest/manifest.yml]"
 set +x
 cat > manifest.yml <<EOF
 ---
 applications:
 - name: ${APP_NAME}
   host: ${APP_HOST}
-  path: ${APP_PATH}
 
 EOF
 
 cat manifest.yml
-cp ../artifact-to-deploy/* .
 
 set -x
