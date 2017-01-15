9ad15e9db52613687e8a01bf93ea03d93e74ce5b
Marcial Rosales <mrosales@pivotal.io>
2017-01-15 18:49:42 +0100

    modify pipeline so that it installs the artifact. Add tasks from previous branch

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# input folders:
+# version : contains a file called number with the current version
+# source-code : contains the source code
+
+# output folders:
+# build: contains the built jar
+
 set -e
 
+source ./pipeline/tasks/common.sh
+
+VERSION=$(build_version "./version" "number" "./source-code" $BRANCH)
+echo "Version to build: ${VERSION}"
+
+echo "Generating maven settings.xml"
 ./pipeline/tasks/generate-settings.sh
 
-cd source-code || echo "missing input resource: source-code"
+cd source-code
 
-echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
+echo "Setting maven with version to build"
+mvn versions:set -DnewVersion=${VERSION}
 
+echo "Building artifact ..."
 mvn verify ${MAVEN_ARGS}
 
-echo "Publishing artifact from target to <output folder: ../build>"
+echo "Copying artifact to ./build "
 cp target/*.jar ../build
