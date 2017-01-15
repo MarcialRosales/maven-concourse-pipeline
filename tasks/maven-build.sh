c0c73200fd6e1fa2af29f40d1458805eaa5cb69a
Marcial Rosales <mrosales@pivotal.io>
2017-01-15 14:30:47 +0100

    maven-build produces a jar with a version

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+# input folders:
+# version : contains a file called number with the current version
+# source-code : contains the source code
+
+# output folders:
+# build: contains the built jar
+
 set -e
+
+source ./pipeline/tasks/common.sh
+
+VERSION=$(build_version ./version number ./source_code $BRANCH)
 
 ./pipeline/tasks/generate-settings.sh
 
 cd source-code || echo "missing input resource: source-code"
 
-if [ -e ../version/number ]; then
-  VERSION=`cat ../version/number`
-  mvn versions:set -DnewVersion=${VERSION}
-fi
+mvn versions:set -DnewVersion=$(VERSION)
 
 echo "Building version ${VERSION} using MAVEN_OPTS: ${MAVEN_OPTS}"
 
 mvn verify ${MAVEN_ARGS}
 
-echo "Publishing artifact from target to <output folder: ../build>"
+echo "Publishing artifact from target to ${build} "
 cp target/*.jar ../build
 
 ls ../build
