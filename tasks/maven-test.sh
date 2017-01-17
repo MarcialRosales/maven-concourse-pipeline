0dbed864ba0005239116193cfd0f2c5290dd13a9
Marcial Rosales <mrosales@pivotal.io>
2017-01-17 18:12:31 +0100

    use baseURI instead of RestAssured.baseURI

--- tasks/maven-test.sh
+++ tasks/maven-test.sh
@@ -9,4 +9,4 @@ cd acceptance-test || echo "missing input resource: acceptance-test"
 APP_URI=https://${APP_HOST}.${APP_DOMAIN}
 
 echo "Running acceptance tests against ${APP_URI}"
-mvn test -DRestAssured.baseURI=${APP_URI} -DRestAssured.port=${APP_PORT:-"80"}
+mvn test -DbaseURI=${APP_URI} 
