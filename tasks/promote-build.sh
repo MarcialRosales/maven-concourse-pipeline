b1ac85785003bbf193a0f627b227633203b468fa
Marcial Rosales <mrosales@pivotal.io>
2017-01-17 20:00:31 +0100

    added new task that promotes an artifact to a new version

---
+++ tasks/promote-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+RELEASE_CANDIDATE_VERSION=$(current_sem_ver "./version" "number")
+
+cd build || echo "missing input resource: build"
+
+BUILD=`ls`
+
+APP_NAME=`echo $BUILD | cut -d '-' -f 1`
+APP_EXTENSION=`echo $BUILD | rev | cut -d '.' -f 1 | rev`
+
+RELEASE_CANDIDATE=$APP_NAME-$RELEASE_CANDIDATE_VERSION.$APP_EXTENSION
+
+echo "Promoting ${BUILD} to ${RELEASE_CANDIDATE}"
+cp $BUILD ../release-candidate
