57c0451d083e67dd87f7c61615571e191e7e086c
Marcial Rosales <mrosales@pivotal.io>
2017-01-19 11:00:31 +0100

    change the way i build the new release candidate file

--- tasks/promote-build.sh
+++ tasks/promote-build.sh
@@ -9,9 +9,8 @@ RELEASE_CANDIDATE_VERSION=$(current_sem_ver "./version" "number")
 cd build || echo "missing input resource: build"
 
 BUILD=`ls`
-
-APP_NAME=`echo $BUILD | cut -d '-' -f 1`
-APP_EXTENSION=`echo $BUILD | rev | cut -d '.' -f 1 | rev`
+APP_EXTENSION="${BUILD##*.}"
+APP_NAME="${BUILD%-*}"
 
 RELEASE_CANDIDATE=$APP_NAME-$RELEASE_CANDIDATE_VERSION.$APP_EXTENSION
 
